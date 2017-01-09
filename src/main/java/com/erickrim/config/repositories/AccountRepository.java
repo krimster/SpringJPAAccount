0bc833bb039ca406e1a121407440fa6914786cf3
krime <erickrim@yahoo.co.uk>
2017-01-09 12:45:22 +0000

    Initial commit

---
+++ src/main/java/com/erickrim/config/repositories/AccountRepository.java
@@ -0,0 +1,21 @@
+package com.erickrim.config.repositories;
+
+
+import com.erickrim.config.entities.Account;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface AccountRepository {
+    List<Account> getAccounts();
+
+    Account getAccount(Long id);
+
+    int getNumberOfAccounts();
+
+    Long createAccount(BigDecimal initialBalance);
+
+    int deleteAccount(Long id);
+
+    void updateAccount(Account account);
+}
